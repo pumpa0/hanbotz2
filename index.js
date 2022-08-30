d933ac138cbbfe4ca6780de4f6be3bca4565ecb3
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-08-30 12:34:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 require('./settings')
 const { default: hanbotzConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
